6ebf89560cd3624e93c4beffa4481dee9b56aff3
Christian de Barrio <christian.debarrio@cabify.com>
2020-02-15 14:37:36 +0100

    add author view

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Christian de Barrio Arribas on 15/02/2020.
+//  Copyright © 2020 Cabify. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+   
+}
