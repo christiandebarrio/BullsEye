fac373ace5be8900c27b6245a2901ef64dc60e86
Christian de Barrio <christian.debarrio@cabify.com>
2020-02-15 13:54:40 +0100

    add action handler to update the score and round

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -52,13 +52,14 @@ class ViewController: UIViewController {
         
         let message = "You scored \(points) points"
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: {
+            action in
+            self.startNewRound()
+        })
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
-        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider:UISlider) {
